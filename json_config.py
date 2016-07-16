87750625ce0961b15f40daf72f6ab22ac6fad033
Mark Stefanovic <markstefanovic.com@gmail.com>
2016-07-16 07:38:55 -0700

    fixed doctest

--- json_config.py
+++ json_config.py
@@ -3,10 +3,6 @@
 This class will only load the file once.  Multiple calls to the configuration
 settings will only result in a single file import.
 
-One of the guiding principles of this package is to minimize file IO. The downside 
-of this is that if multiple processes use the same config file, they may retrieve 
-outdated results since the configuration file is only loaded once.
-
 Examples:
 >>> cfg = SimpleJsonConfig('test.config')
 >>> cfg.get_or_set_variable('hello', 1)
@@ -19,22 +15,24 @@ Examples:
 [1, 2, 3]
 
 >>> cfg.get_variable('test2')
-
+Traceback (most recent call last):
+...
+KeyError: 'test2'
 """
 import json
 import os
-from typing import Dict, TypeVar
+from typing import Dict, TypeVar, Union
 
 from PyQt4 import QtCore
 
 
-JsonType = TypeVar('JsonType', float, int, list, str)
+JsonType = Union[float, int, list, str]
 
 
 class SimpleJsonConfig(QtCore.QObject):
     error_signal = QtCore.pyqtSignal(str)
 
-    def __init__(self, json_path='config\config.json') -> None:
+    def __init__(self, json_path: str='config\config.json') -> None:
         """Initialize an instance of the SimpleJsonConfig class."""
         super(SimpleJsonConfig, self).__init__()
         self.json_path = json_path
@@ -44,21 +42,25 @@ class SimpleJsonConfig(QtCore.QObject):
     def all_variables(self) -> dict:
         return self._cache
 
-    def set_variable(self, key, val) -> None:
+    def set_variable(self, key: str, val: JsonType) -> None:
         """Set the value of the configuration variable for the key specified and save it to disk.
 
         If the key value specified doesn't exist, then it will be created.  If the value specified
         matches the pre-existing value, then no IO will happen.
         """
-        if self.variable(key) != val:
+        try:
+            if self.get_variable(key) != val:
+                self._cache[key] = val
+                self.save()
+        except:
             self._cache[key] = val
             self.save()
 
-    def get_variable(self, key) -> JsonType:
+    def get_variable(self, key: str) -> JsonType:
         """Lookup a configuration variable's value using the variable's name."""
-        return self.all_variables.get(key)
+        return self.all_variables[key]
 
-    def get_or_set_variable(self, key, default_value) -> JsonType:
+    def get_or_set_variable(self, key: str, default_value: JsonType) -> JsonType:
         """Lookup the value of a configuration variable by name.  If it doesn't exist create it."""
         try:
             return self.get_variable(key)
@@ -66,17 +68,17 @@ class SimpleJsonConfig(QtCore.QObject):
             self.set_variable(key, default_value)
         return default_value
 
-    def load(self) -> dict:
+    def load(self) -> Dict[str, JsonType]:
         """Read json file into a dictionary."""
         try:
             with open(os.path.abspath(self.json_path), 'r') as fh:
                 return json.load(fh)
         except FileNotFoundError:
             self.error_signal.emit("Config file config.json not found in current directory")
-            return {}
+            return {'': 0}
         except Exception as e:
             self.error_signal.emit("Configuration error: {}".format(e))
-            return {}
+            return {'': 0}
 
     def save(self) -> None:
         """Save all configuration variables to disk."""
@@ -89,6 +91,7 @@ class SimpleJsonConfig(QtCore.QObject):
 
 
 if __name__ == "__main__":
+    cfg.get_variable('test2')
     import doctest
     doctest.testmod()
 
