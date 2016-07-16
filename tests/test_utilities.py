fde897b2704214f44a4417bc69b98982b5b33a90
Mark Stefanovic <markstefanovic.com@gmail.com>
2016-07-15 19:32:54 -0700

    initial commit

---
+++ tests/test_utilities.py
@@ -0,0 +1,45 @@
+import os
+import sqlite3
+
+import pytest
+
+from utilities import inspect_table, is_float, SqliteField, valid_sql_field_name
+
+
+def setup_module(module):
+    with sqlite3.connect('test.db') as con:
+        con.execute("CREATE TABLE {t} ({f} INTEGER PRIMARY KEY)".format(t='test', f='id'))
+        con.commit()
+
+
+def teardown_module(module):
+    os.remove('test.db')
+
+
+def test_inspect_table():
+    assert inspect_table(db='test.db', table='test') == [
+        SqliteField(
+            ix=0, name='id'
+            , data_type='INTEGER'
+            , nullable=False
+            , default_value=None
+            , primary_key=True
+        )
+    ]
+
+
+def test_is_float():
+    assert is_float('a') == False
+    assert is_float(1.0) == True
+    assert is_float(-1) == True
+    assert is_float("'") == False
+
+
+def test_valid_sql_field_name():
+    assert valid_sql_field_name('0123_asdf') == False
+    assert valid_sql_field_name('customer_id') == True
+    assert valid_sql_field_name("'") == False
+    assert valid_sql_field_name("_") == False
+
+if __name__ == '__main__':
+    pytest.main(__file__)
